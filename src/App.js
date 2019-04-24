3216fda54de7ee73ce4dfaf6194ff54b06673c23
Doug <me@douglasbowen.com>
2019-04-24 17:16:20 -0400

    rsvp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
 
 const todoArray = [
   {
@@ -28,11 +30,54 @@ class App extends React.Component {
       }
     };
   }
+
+  addTodo = event => {
+    event.preventDefault();
+    this.setState({
+      todoData: [...this.state.todoData, this.state.newTodo],
+      newTodo: {
+        task: "",
+        id: "",
+        completed: ""
+      }
+    });
+  };
+
+  handleChanges = event => {
+    console.log(event.target.name);
+    this.setState({
+      newTodo: {
+        ...this.state.newTodo,
+        task: event.target.value
+      }
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
         <p>test</p>
+        {/*
+        <div className="Todo-List">
+          {this.state.todoData.map(todo => (
+            <Todo todo={todo} />
+          ))}
+        </div>
+	     */}
+        <TodoList todoData={this.state.todoData} />
+        {/*   
+        <form onSubmit={this.addTodo}>
+          <input
+            placeholder="yourtodo"
+            value={this.state.newTodo.task}
+            onChange={this.handleChanges}
+            name="yourtodo"
+          />
+          <button>Add todo</button>
+        </form>
+	     */}
+        <TodoForm newTodo={this.state.newTodo} what={this} />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo.js";
+/*
+const TodoList = props => {
+    return <div className="todo-list">
+	{props.todoData.map(todo => (
+	    <Todo 
+	</div>;
+}
+*/
+function TodoList(props) {
+  return (
+    <div className="Todo-List">
+      {props.todoData.map(todo => (
+        <Todo todo={todo} />
+      ))}
+    </div>
+  );
+}
+
+export default TodoList;
